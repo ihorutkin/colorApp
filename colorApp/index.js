1177b2b418e69b76cbcbbb40a80d24420cd35c6e
ihorutkin <85029363+ihorutkin@users.noreply.github.com>
2022-11-12 16:18:17 +0100

    Add files via upload
    This is an application that creates a palette of random colors that can be passed by reference using a hash, works with the help of technology such as Live Stream

---
+++ colorApp/index.js
@@ -0,0 +1,92 @@
+const cols = document.querySelectorAll('.col')
+
+document.addEventListener('keydown', event => {
+    event.preventDefault()
+    if(event.code.toLowerCase() === 'space'){
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', event => {
+    const type = event.target.dataset.type
+    if(type === "lock"){
+        const node = event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0]
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    }
+    else if(type ==='copy'){
+        copyToClipboard(event.target.textContent)
+    }
+})
+
+
+
+function generateColor(){
+    let str = '0123456789ABCDEF'
+
+    let color = ''
+
+    for(let i = 0; i < 6; i++){
+        color += str[Math.floor(Math.random() * str.length)]
+    }
+
+    return '#' + color
+}
+
+function copyToClipboard(text){
+    return navigator.clipboard.writeText(text)
+}
+
+function setRandomColors(isInitial){
+    const colors = isInitial ? getColorsFromHash() : []
+    cols.forEach((col, index) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const btn = col.querySelector('button')
+
+        if(isLocked){
+            colors.push(text.textContent)
+            return
+        }
+
+        const color = isInitial 
+        ? colors[index] 
+            ? colors[index] 
+            : chroma.random() 
+        : chroma.random()
+
+        if(!isInitial){
+            colors.push(color)
+        }
+
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(btn, color)
+    })
+
+    updateColorsHash(colors)
+}
+
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+function updateColorsHash(colors = []){
+    document.location.hash = colors.map(col => {
+        return col.toString().substring(1)
+    }).join('-')
+}
+
+function getColorsFromHash(){
+    if(document.location.hash.length > 1){
+        return document.location.hash.substring(1).split('-').map(color => '#' + color)
+    }
+    return []
+}
+
+setRandomColors(true)
